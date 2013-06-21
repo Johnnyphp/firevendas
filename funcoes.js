2100593ff92343e6d8b51e9a4c2c9821367a67b2
Johnny <firevendas@firevendas.com.br>
2013-06-21 19:08:44 -0300

    teste
    teste

--- funcoes.js
+++ funcoes.js
@@ -1,5 +1,39 @@
-﻿function autenticaUsuario(){
+﻿function verificaTabelas(){
+
+	console.log('VERIFICA TABELAS');
 	
+	var repres = localStorage.getItem('codigo_user');
+	var empresa = localStorage.getItem('empresa_user');
+	
+	$.ajax({
+				
+		type: "POST",
+		url: "http://www.firevendas.com.br/sistema/empresa/tarefas/sinc_representante.php",
+		data: "repres="+repres+"&empresa="+empresa,
+				
+		success: function(retorno){
+			
+			alert(retorno);
+			
+			if(retorno == '')
+				console.log('Não foi possível sincronizar as tabelas, sem acesso a internet.');
+			else
+				console.log('Tabelas verificadas');
+		}
+		
+	});
+	
+}
+
+function autenticaUsuario(){
+	
+	localStorage.setItem('id_user', '');
+	localStorage.setItem('nome_user', '');
+	localStorage.setItem('codigo_user', '');
+	localStorage.setItem('email_user', '');
+	localStorage.setItem('senha_user', '');
+	localStorage.setItem('empresa_user', '');
+			
 	var temp = localStorage.getItem('tb_usuarios');
 	var linhas = temp.split('#');
 	var num_linhas = linhas.length;
@@ -7,24 +41,29 @@
 		
 	for(var i=0; i<= num_linhas; i++){
 	
-		campos = linhas[i].split('|');
-		console.log(campos[3]);
-		if(campos[3] == $('#email').val() && campos[4] == $('#senha').val()){
-		
-			VerificaTabelas();	
-			break;
+		if(linhas[i] != null){
 			
+			console.log('TESTE SPLIT: '+linhas[i]);
+			campos = linhas[i].split('|');
+	
+			if(campos[3] == $('#email').val() && campos[4] == $('#senha').val()){
+			
+				localStorage.setItem('id_user', campos[0]);
+				localStorage.setItem('nome_user', campos[1]);
+				localStorage.setItem('codigo_user', campos[2]);
+				localStorage.setItem('email_user', campos[3]);
+				localStorage.setItem('senha_user', campos[4]);
+				localStorage.setItem('empresa_user', campos[5]);
+				verificaTabelas();
+				
+				break;
+				
+			}
 		}
-		
-		/*
-		alert('ID: '+campos[0]);
-		alert('NOME: '+campos[1]);
-		alert('CPF/CNPJ: '+campos[2]);
-		alert('EMAIL: '+campos[3]);
-		alert('SENHA: '+campos[4]);
-		*/
-		
 	}
+	
+	if(localStorage.getItem('id_user') == '')
+		alert('Usuario não encontrado!');
 	
 }
 
@@ -42,7 +81,7 @@ function verificaNovoUser(){
 
 function cadastraNovoUser(){
 
-		var novo_user = '1|'+$('#nome').val()+'|'+$('#cpfcnpj').val()+'|'+$('#email_cad').val()+'|'+$('#senha_cad').val()+'#';
+		var novo_user = '1|'+$('#nome').val()+'|'+$('#codigo').val()+'|'+$('#email_cad').val()+'|'+$('#senha_cad').val()+'|'+$('#codigo_empresa').val()+'#';
 		localStorage.setItem('tb_usuarios', novo_user);
 		$('#primeiro_cadastro').hide();
 	
