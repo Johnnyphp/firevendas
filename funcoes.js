8aa0bbeafb97987606712f6d4b2b72a45ed4e9dd
Johnny <firevendas@firevendas.com.br>
2013-06-24 16:19:45 -0300

    teste
    teste

--- funcoes.js
+++ funcoes.js
@@ -1,5 +1,46 @@
-﻿function verificaTabelas(){
+﻿var url_txt;
+function IrUrl(url_txt){
 
+	window.location = url_txt;
+	
+}
+
+function SincronizaCatalogo(){
+
+	$('#carregando_1').show();
+	var repres = localStorage.getItem('codigo_user');
+	var empresa = localStorage.getItem('empresa_user');
+	
+	$.ajax({
+				
+		type: "POST",
+		url: "http://www.firevendas.com.br/sistema/empresa/tarefas/sinc_catalogo.php",
+		data: "repres="+repres+"&empresa="+empresa,
+				
+		success: function(retorno){
+			
+			if(retorno == ''){
+
+				$('#msg_erro').show();
+				$('#carregando_1').hide();
+			
+			}else{
+				
+				$('#carregando_1').hide();
+				$('#msg_sucesso').show();	
+			}
+			
+			
+			
+			
+		}
+	});
+}
+
+function verificaTabelas(){
+
+	$('#carregando_1').show();
+	
 	console.log('VERIFICA TABELAS');
 	
 	var repres = localStorage.getItem('codigo_user');
@@ -13,12 +54,80 @@
 				
 		success: function(retorno){
 			
-			alert(retorno);
+			console.log(retorno);
 			
-			if(retorno == '')
+			if(retorno == ''){
 				console.log('Não foi possível sincronizar as tabelas, sem acesso a internet.');
-			else
-				console.log('Tabelas verificadas');
+				localStorage.setItem('status_user', 'Você esta offline.');
+			}else{
+				
+				localStorage.setItem('status_user', 'Você esta online.');
+				
+				//Monta um Estrutura de dados Local
+				var tabelas = retorno.split('----');
+				var quant_tabelas = tabelas.length;
+				
+				console.log('TOTAL DE TABELAS: '+quant_tabelas);
+				
+				for(var i=0; i<= quant_tabelas; i++){
+					
+					console.log('Nº TABELA '+i);
+					
+					if(i == 0){ //Tabela Clientes
+						
+						localStorage.setItem('tb_clientes', tabelas[i]);
+						//console.log('GRAVOU: tb_clientes');
+						
+						
+					}else if(i == 1){ //Tabela Produtos
+						
+						localStorage.setItem('tb_produtos', tabelas[i]);
+						//console.log('GRAVOU: tb_produtos');
+						
+					}else if(i == 2){ //Tabela de Preços
+						
+						localStorage.setItem('tb_tabela_de_precos', tabelas[i]);
+						//console.log('GRAVOU: tb_tabela_de_precos');
+						
+					}else if(i == 3){ //Tabela de Marcas
+						
+						localStorage.setItem('tb_marcas', tabelas[i]);
+						//console.log('GRAVOU: tb_marcas');
+						
+					}else if(i == 4){ //Tabela de Tipos de Documentos / Local Cobrança
+						
+						localStorage.setItem('tb_tipo_documento', tabelas[i]);
+						//console.log('GRAVOU: tb_tipo_documento');
+						
+					}else if(i == 5){ //Tabela de Comissões
+						
+						localStorage.setItem('tb_tabela_comissoes', tabelas[i]);
+						//console.log('GRAVOU: tb_tabela_comissoes');
+						
+					}else if(i == 6){ //Tabela Condições de Pagamento
+						
+						localStorage.setItem('tb_condicoes_de_pagamento', tabelas[i]);
+						//console.log('GRAVOU: tb_condicoes_de_pagamento');
+						
+					}else if(i == 7){ //Tabela Percentual Solado
+						
+						localStorage.setItem('tb_percentual_solado', tabelas[i]);
+						//console.log('GRAVOU: tb_percentual_solado');
+						
+					}
+					
+					console.log(tabelas[i]);
+					
+				}
+				
+				console.log('MOSTRANDO CATALOGO DIGITAL: '+localStorage.getItem('tb_catalogo_digital'));
+				console.log('Verificação de Tabelas Concluídas.');
+		
+			}
+			
+			$('#carregando_1').hide();
+			window.location = 'painel.html';
+
 		}
 		
 	});
